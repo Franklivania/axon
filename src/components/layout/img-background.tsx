32ad81cc2dbc871ebaa8777e5e9be3e733b8cf47
Chibuzo Franklin Odigbo <cfaodigbo@gmail.com>
2026-03-09 21:56:10 +0100

    fix: update image display for themes

--- src/components/layout/img-background.tsx
+++ src/components/layout/img-background.tsx
@@ -1,15 +1,22 @@
-"use client";
-
-import { useTheme } from "next-themes";
 import Image from "next/image";
 
 export default function ImageBackground() {
-  const { theme } = useTheme();
-
-  const isDark = theme === "dark";
-  const logoSrc = isDark ? "/bg-dark.webp" : "/bg-light.webp";
-
   return (
-    <Image src={logoSrc} fill alt="Axon Background" className="object-cover" />
+    <>
+      <Image
+        src="/bg-dark.webp"
+        fill
+        alt="Axon Background"
+        className="object-cover hidden dark:block"
+        loading="eager"
+      />
+      <Image
+        src="/bg-light.webp"
+        fill
+        alt="Axon Background"
+        className="object-cover block dark:hidden"
+        loading="eager"
+      />
+    </>
   );
 }
